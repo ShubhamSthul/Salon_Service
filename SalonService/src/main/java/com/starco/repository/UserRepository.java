0021b3d6a6fb39b2260b97343e2e6f8ab79851fa
ShubhamSthul <sthul.shubham123@gmail.com>
2025-03-14 20:11:59 +0530

    salon module deleted

--- SalonService/src/main/java/com/starco/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.starco.repository;
-
-import com.starco.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
